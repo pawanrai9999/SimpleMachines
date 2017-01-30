6b98771d420546749a865b245d90e3831b245bca
pawanrai9999 <pawanrai9999@gmail.com>
2017-01-30 23:24:41 +0530

    some edit

--- detectFlower.py
+++ detectFlower.py
@@ -7,6 +7,7 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from sklearn.svm import SVC
+#from sklearn import tree
 from sklearn.metrics import accuracy_score
 
 # Switch to your matplotlib backend to show output
@@ -16,6 +17,10 @@ plt.switch_backend('Qt5Agg');
 irisDataset = datasets.load_iris()
 #Shaping data for use with numpy
 irisDataset.data.shape, irisDataset.target.shape
+
+print('X: {0} Y: {1}'.format(irisDataset.data[0], irisDataset.target[0]))
+print('X: {0} Y: {1}'.format(irisDataset.data[50], irisDataset.target[50]))
+print('X: {0} Y: {1}'.format(irisDataset.data[105], irisDataset.target[105]))
 #Splliting data
 X_train, X_test, Y_train, Y_test = train_test_split(irisDataset.data,
                                                     irisDataset.target,
@@ -25,10 +30,11 @@ X_train, X_test, Y_train, Y_test = train_test_split(irisDataset.data,
 X_train.shape, Y_train.shape
 X_test.shape, Y_test.shape
 
-
 # Initializing classifier
-clf = SVC(kernel='linear')
+#clf = tree.DecisionTreeClassifier()
+clf = SVC(C=1, kernel='linear')
 # Training classifier
 clf.fit(X_train, Y_train)
-score = clf.score(X_test, Y_test)
+prediction = clf.predict(X_test)
+score = accuracy_score(Y_test, prediction)
 print('Score: {}'.format(score))
