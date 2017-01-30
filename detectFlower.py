fe0e0010260ce9d1178f0cce5daf05302b8f6bd2
pawanrai9999 <pawanrai9999@gmail.com>
2017-01-30 21:15:17 +0530

    Simple iris dataset pridiction program

---
+++ detectFlower.py
@@ -0,0 +1,8 @@
+# This program uses support vector machine of scikit-learn as classifier
+# Data used here is provided by scikit-learn
+# Features used
+
+import numpy as np
+import sklearn.dataset.load_iris as data
+import matplotlib.pyplot as plt
+import sklearn.svm as svm
