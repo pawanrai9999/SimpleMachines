8b017fb03051667f7cc19ffe45b2fcc192c0578f
pawanrai9999 <pawanrai9999@gmail.com>
2017-02-03 19:51:51 +0530

    Use of knn_classifier

---
+++ KNN_Classifier.py
@@ -0,0 +1,23 @@
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
+
+digits = load_digits()
+X = digits.data
+#np.array([[2, 3], [5, 8], [3, 8], [6, 6], [5, 2]])
+y = digits.target
+#np.array([0, 0, 0, 1, 1])
+
+#Splitting datasets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=40)
+
+# Initializing classifier
+clf = KNeighborsClassifier(n_neighbors=3)
+clf = clf.fit(X_train, y_train)
+
+predict = clf.predict(X_test)
+score = accuracy_score(y_test, predict)
+
+print("Answer: {0}\nScore: {1}".format(predict, score))
